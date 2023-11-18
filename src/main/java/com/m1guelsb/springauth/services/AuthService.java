b65b64036b3b8efd35eeddc98eecfa0f9060e0ce
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-11-18 00:07:56 -0300

    [feat] add auth service

---
+++ src/main/java/com/m1guelsb/springauth/services/AuthService.java
@@ -0,0 +1,46 @@
+package com.m1guelsb.springauth.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.m1guelsb.springauth.dtos.SignUpDto;
+import com.m1guelsb.springauth.entities.User;
+import com.m1guelsb.springauth.exceptions.InvalidJwtException;
+import com.m1guelsb.springauth.repositories.UserRepository;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    var user = repository.findByLogin(username);
+
+    if (user != null) {
+      return user;
+    } else {
+      throw new UsernameNotFoundException("Wrong credentials");
+    }
+
+  }
+
+  public UserDetails signUp(SignUpDto data) throws InvalidJwtException {
+
+    if (repository.findByLogin(data.login()) != null) {
+      throw new InvalidJwtException("Username already exists");
+    }
+
+    String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+    User newUser = new User(data.login(), encryptedPassword, data.role());
+
+    return repository.save(newUser);
+
+  }
+}
